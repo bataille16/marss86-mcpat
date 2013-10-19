90b2d4dac258b41b298613245b85b101c232b15d
Bataille16 <bataille16@gmail.com>
2013-10-19 01:57:06 -0400

    fixed some bugs

--- cache/controller.h
+++ cache/controller.h
@@ -1,4 +1,4 @@
-RSSx86 : A Full System Computer-Architecture Simulator
+/*MARSRSSx86 : A Full System Computer-Architecture Simulator
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
